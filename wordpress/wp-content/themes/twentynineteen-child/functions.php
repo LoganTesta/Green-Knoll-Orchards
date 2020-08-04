fb1ffeb2f6349dd397d34d781aba76b93f707208
logan <logan.testa@outlook.com>
2020-08-03 17:01:23 -0700

    Limit access to JSON data

--- wordpress/wp-content/themes/twentynineteen-child/functions.php
+++ wordpress/wp-content/themes/twentynineteen-child/functions.php
@@ -5,3 +5,15 @@ add_image_size( "medium_square_crop", 400, 400, true );
 add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css' );
 });
+
+
+/*Prevent non-logged in users from accessing the site's user and post data using the WordPress REST API.*/
+add_filter( 'rest_authentication_errors', function( $result ) {
+   if ( !empty( $result ) ) {
+       return $result;
+   } 
+   if( !is_user_logged_in() ) {
+       return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );    
+   }
+   return $result;
+});
