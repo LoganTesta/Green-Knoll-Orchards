f98ccb2cfaeaf06757d4154286cd9325b46bcea1
logan <logan.testa@outlook.com>
2022-05-27 21:50:48 -0700

    Enqueue print styles, indent entry-header

--- wordpress/wp-content/themes/twentynineteen-child/functions.php
+++ wordpress/wp-content/themes/twentynineteen-child/functions.php
@@ -16,4 +16,5 @@ add_image_size( "medium_square_crop", 400, 400, true );
 
 add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=05242022' );
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=05272022' );
 });
