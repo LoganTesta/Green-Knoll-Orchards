1bf469c140ca2eea83c5ed44806de57f5cf6989b
logan <logan.testa@outlook.com>
2022-05-29 11:39:49 -0700

    Reformat link styling for print, adjust enqueue for print only

--- wordpress/wp-content/themes/twentynineteen-child/functions.php
+++ wordpress/wp-content/themes/twentynineteen-child/functions.php
@@ -15,6 +15,6 @@
 add_image_size( "medium_square_crop", 400, 400, true );
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=05242022' );
-    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=05272022' );
+    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=05292022' );
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=05292022', array(), '', 'print' );
 });
