05f40bd4cac18911fb6666168b19277014083a0f
logan <logan.testa@outlook.com>
2022-10-08 20:54:13 -0700

    Adjust column spacing for General Testimonials plugin

--- wordpress/wp-content/themes/twentynineteen-child/functions.php
+++ wordpress/wp-content/themes/twentynineteen-child/functions.php
@@ -16,6 +16,6 @@ add_image_size( "medium_square_crop", 400, 400, true );
 
 add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'main-styles', "" . get_stylesheet_directory_uri() . '/assets/css/main-styles.css?mod=10072022');
-    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=10072022', array(), '', 'print' );
+    wp_enqueue_style( 'main-styles', "" . get_stylesheet_directory_uri() . '/assets/css/main-styles.css?mod=10082022');
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=10082022', array(), '', 'print' );
 });
