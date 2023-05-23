e2fc60af1ed515cb6f65aa318bb84ae3de37dfa7
logan <logan.testa@outlook.com>
2023-05-22 22:10:23 -0700

    Clean up functions.php

--- wordpress/wp-content/themes/twentynineteen-child/functions.php
+++ wordpress/wp-content/themes/twentynineteen-child/functions.php
@@ -1,17 +1,5 @@
 <?php
 
-/*Prevent non-logged in users from accessing the site's user and post data using the WordPress REST API.*/
-//add_filter( 'rest_authentication_errors', function( $result ) {
-//   if ( !empty( $result ) ) {
-//       return $result;
-//   } 
-//   if( !is_user_logged_in() ) {
-//       return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );    
-//   }
-//   return $result;
-//});
-
-
 add_image_size( "medium_square_crop", 400, 400, true );
 
 add_action( 'wp_enqueue_scripts', function() {
