28d0d0ce8a8be6e935e347e9a71d9d3920126c54
logan <logan.testa@outlook.com>
2023-06-05 22:23:36 -0700

    Add positioning for search form

--- wordpress/wp-content/themes/twentynineteen-child/functions.php
+++ wordpress/wp-content/themes/twentynineteen-child/functions.php
@@ -4,6 +4,6 @@ add_image_size( "medium_square_crop", 400, 400, true );
 
 add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'main-styles', "" . get_stylesheet_directory_uri() . '/assets/css/main-styles.css?mod=11012022');
-    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=11012022', array(), '', 'print' );
+    wp_enqueue_style( 'main-styles', "" . get_stylesheet_directory_uri() . '/assets/css/main-styles.css?mod=06052023');
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=06052023', array(), '', 'print' );
 });
