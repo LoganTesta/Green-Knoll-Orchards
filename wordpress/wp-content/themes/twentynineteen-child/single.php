c9dabad6bccad6bac64fc086c6b0e7afcbff306f
logan <logan.testa@outlook.com>
2023-06-15 21:02:09 -0700

    Add custom single post page layout

---
+++ wordpress/wp-content/themes/twentynineteen-child/single.php
@@ -0,0 +1,32 @@
+<?php
+
+/*Template Name: Single post custom template
+ Description: the customized single post page layout for this site. */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area <?php echo strtolower(get_the_title()); ?>">
+		<main id="main" class="site-main">
+
+			<?php
+
+			// Start the Loop.
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content/content', '' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+			endwhile; // End the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
