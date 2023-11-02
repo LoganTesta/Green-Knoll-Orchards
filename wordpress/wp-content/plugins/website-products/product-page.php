0870968891af4425ef255384de24caead7b73e39
logan <logan.testa@outlook.com>
2023-11-01 21:32:28 -0700

    Build out start of product-page for website-products

--- wordpress/wp-content/plugins/website-products/product-page.php
+++ wordpress/wp-content/plugins/website-products/product-page.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+/* Generic layout for the custom post type for this plugin. */
 
+get_header();
 ?>
+
+    <div class="website-products-product-content">
+        <h1><?php echo get_the_title(); ?></h1>
+    </div>
+
+<?php
+get_footer();
